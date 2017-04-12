0620404b881783080f797833a1cc6eda27f56dc7
minkeetan486 <tminkee@yahoo.com>
2017-04-12 10:57:03 +0800

    Completed Login Operation

---
+++ app/src/main/java/com/example/wbf486/androidlibrary/LoginRequest.java
@@ -0,0 +1,34 @@
+package com.example.wbf486.androidlibrary;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Created by WBF486 on 4/11/2017.
+ */
+
+public class LoginRequest extends StringRequest{
+    private static final String LOGIN_REQUEST_URL = "https://androidlibrary.000webhostapp.com/LibraryApp/development/login.php";
+    private Map<String, String> param;
+    private static final String TAG = "LibraryApp";
+
+    public LoginRequest(String coreid, String password, Response.Listener<String> listener){
+        super(Request.Method.POST, LOGIN_REQUEST_URL, listener, null);
+        param = new HashMap<>();
+        param.put("version", "1");
+        param.put("coreid", coreid);
+        param.put("password", password);
+        Log.d(TAG, "LoginRequest: created");
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return param;
+    }
+}
--- app/src/main/java/com/example/wbf486/androidlibrary/MainActivity.java
+++ app/src/main/java/com/example/wbf486/androidlibrary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wbf486.androidlibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String str_fullName = intent.getStringExtra("fullName");
+        boolean b_isAdmin = intent.getBooleanExtra("isAdmin", false);
+
+        String message = str_fullName + ", Welcome to Astro Library";
+        EditText eTWelcome = (EditText) findViewById(R.id.eTWelcome);
+        eTWelcome.setText(message);
     }
+
+
 }

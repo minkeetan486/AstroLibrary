fa9bb2c24060522cfcf8c9cc1b0f589641fea8f3
mugun88 <password54321>
2017-05-06 18:14:33 +0800

    Added change for library list

---
+++ app/src/main/java/com/example/xjh786/mainpage/LibraryList.java
@@ -0,0 +1,13 @@
+package com.example.xjh786.mainpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibraryList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_list);
+    }
+}
